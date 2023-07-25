21520418684dfd443f328b546e216fde22df3cc1
bartek33642 <92989898+bartek33642@users.noreply.github.com>
2023-07-25 15:39:51 +0200

    first commit

--- client/src/components/dashboard/admin/AdminDashboard.jsx
+++ client/src/components/dashboard/admin/AdminDashboard.jsx
@@ -1,83 +1,23 @@
 import React from "react";
 import './AdminDasboard.css';
 import { Menu } from "../../menu/Menu";
-
 export function AdminDashboard(){
+
     return(        
-        <div>
-            <Menu />         
-        <div>
-            <h1>Admin dashboard</h1>
-            <div className="content-admin">
-                <button className="button-box">{/* 1 */}
-                    <div className="box">
-                    <div className="box-icon">
-                        <img src="https://img.icons8.com/?size=512&id=23264&format=png" alt="user"/>
-                    </div>
-                    <div className="box-title">
-                        <h3>Użytkownicy</h3>
-                    </div>
-                </div>
-                </button>
+        <div className="dashboard">
+            <Menu />        
+            <div className="dasboard-content">
+                <h3>Witamy w panelu administratora</h3>
 
-                <button className="button-box">{/* 2 */}
-                <div className="box">
-                    <div className="box-icon">
-                        <img src="https://img.icons8.com/?size=512&id=23264&format=png" alt="user"/>
-                    </div>
-                    <div className="box-title">
-                        <h3>Ustawienia</h3>
-                    </div>
+                <div>
+                    
                 </div>
-                </button>
-
-                <button className="button-box">{/* 3 */}
-                <div className="box">
-                    <div className="box-icon">
-                        <img src="https://img.icons8.com/?size=512&id=23264&format=png" alt="user"/>
-                    </div>
-                    <div className="box-title">
-                        <h3>Narzędzia</h3>
-                    </div>
-                </div>
-                </button>
-
-                <button className="button-box">{/* 4 */}
-                <div className="box">
-                    <div className="box-icon">
-                        <img src="https://img.icons8.com/?size=512&id=23264&format=png" alt="user"/>
-                    </div>
-                    <div className="box-title">
-                        <h3>Lalal</h3>
-                    </div>
-                </div>
-                </button>
-
-                <button className="button-box">{/* 5 */}
-                <div className="box">
-                    <div className="box-icon">
-                        <img src="https://img.icons8.com/?size=512&id=23264&format=png" alt="user"/>
-                    </div>
-                    <div className="box-title">
-                        <h3>BlaBalBla</h3>
-                    </div>
-                </div>
-                </button>
-
-                <button className="button-box">{/* 6 */}
-                <div className="box">
-                    <div className="box-icon">
-                        <img src="https://img.icons8.com/?size=512&id=23264&format=png" alt="user"/>
-                    </div>
-                    <div className="box-title">
-                        <h3>Gadagada</h3>
-                    </div>
-                </div>
-                </button>
-
-            </div>
-        </div>
-        </div>
+            </div> 
+            
+        
+       </div>
+       
+        
 
     );
 }
--- client/src/components/menu/Menu.jsx
+++ client/src/components/menu/Menu.jsx
@@ -1,74 +1,27 @@
 import React from "react";
 import "./Menu.css";
-
+import { FiHome, FiUser, FiBookOpen, FiCalendar, FiClipboard, FiSettings, FiPower, FiTrello, FiUserPlus, FiUserMinus, FiLayers, FiGrid } from "react-icons/fi";
+import { Link } from "react-router-dom";
 export function Menu(){
+  let imie = "Zenon";
     return (
-      <div class="vertical-menu">
-
-<ul>
-        <li><a class="active" href="/">Home</a></li>
-        <li><a href="/news">News</a></li>
-        <li><a href="/contact">Contact</a></li>
-        <li><a href="/about">About</a></li>
-  </ul>
-      </div>
+      
+<div className="navbar">
+     <ul>
+      <p className="welcomeText">Witaj, {imie}</p>
+      <Link to='/home' className="LinkBtn"><li><FiHome className="Fi"/> Strona główna</li></Link>
+      <Link to='/users' className="LinkBtn"><li> <FiUser className="Fi" /> Użytkownicy</li></Link>
+      <Link to='/classes' className="LinkBtn"><li> <FiGrid className="Fi" /> Klasy</li></Link>
+      <Link to='/grades' className="LinkBtn"><li> <FiBookOpen className="Fi" /> Oceny</li></Link>
+      <Link to='/schedule' className="LinkBtn"><li> <FiCalendar className="Fi" /> Plan zajęć</li></Link>
+      <Link to='/topics' className="LinkBtn"><li> <FiClipboard className="Fi" /> Tematy</li></Link>
+      <Link to='/attendance' className="LinkBtn"><li> <FiUserPlus className="Fi" /> Frekwencja</li></Link>
+      <Link to='/remarks' className="LinkBtn"><li> <FiUserMinus className="Fi" /> Uwagi</li></Link>
+      <Link to='/polls' className="LinkBtn"><li> <FiTrello className="Fi" /> Ankiety</li></Link>
+      <Link to='/subjects' className="LinkBtn"><li> <FiLayers className="Fi" /> Przedmioty</li></Link>
+      <Link to='/settings' className="LinkBtn"><li className="twoMenuBtns"><FiSettings className="Fi" /> Ustawienia</li></Link>
+      <Link to='/' className="LinkBtn"><li className="twoMenuBtns"><FiPower className="Fi" /> Wyloguj</li></Link>
+     </ul>
+    </div>
     );
 }
-
-// import React from 'react';
-// import { Link, Route, Routes } from 'react-router-dom';
-// import { Home } from '../homepage/Home';
-
-// export function Menu() {
-//     return (
-//         <div style={{ display: 'flex' }}>
-//       <div
-//         style={{
-//           width: '200px',
-//           backgroundColor: 'navy',
-//           color: 'white',
-//           padding: '20px',
-//         }}
-//       >
-//         <ul style={{ listStyleType: 'none', textDecoration: 'none', backgroundColor: 'navy', padding: 0 }}>
-//           <li>
-//             <Link to="/">Strona główna</Link>
-//           </li>
-//           <li>
-//             <Link to="/users">Użytkownicy</Link>
-//           </li>
-//           <li>
-//             <Link to="/classes">Klasy</Link>
-//           </li>
-//           <li>
-//             <Link to="/grades">Oceny</Link>
-//           </li>
-//           <li>
-//             <Link to="/schedule">Plan zajęć</Link>
-//           </li>
-//           <li>
-//             <Link to="/attendance">Frekwencja</Link>
-//           </li>
-//           <li>
-//             <Link to="/settings">Ustawienia</Link>
-//           </li>
-//         </ul>
-//         <button style={{ marginBottom: '20px' }}>Wyloguj</button>
-//       </div>
-//       <div style={{ flex: 1, padding: '20px' }}>
-//               <Routes>
-//                 <Route path="/" element={<Home />} />
-//                 <Route path="/users" element="" />
-//                 <Route path="/classes" element="" />
-//                 <Route path="/grades" element="" />
-//                 <Route path="/schedule" element="" />
-//                 <Route path="/attendance" element="" />
-//                 <Route path="/settings" element="" />
-//               </Routes>
-//             </div>
-//           </div>
-//       );
-//     };
-
-    
-   
